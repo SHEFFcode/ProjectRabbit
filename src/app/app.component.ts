2b2e8130bf8a668e1dcb6d80a7fc65247a539df5
SHEFFcode <sheff@sheffmachine.com>
2016-10-13 17:33:36 -0700

    add in a header and a footer area

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from "./header/header.component";
+import {FooterComponent} from "./footer/footer.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css']
+  styleUrls: ['app.component.css'],
+  directives: [HeaderComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'app works!';

1860e7c49611c85cdd3a55c101a9ba998b070989
SHEFFcode <sheff@sheffmachine.com>
2016-10-16 18:27:08 -0700

    create team components

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit } from '@angular/core';
+import {Bracket} from "../legues/bracket";
+import {BrakcetItemComponent} from "../bracket/brakcet-item.component";
 
 @Component({
   moduleId: module.id,
   selector: 'app-sidebar',
   templateUrl: 'sidebar.component.html',
-  styleUrls: ['sidebar.component.css']
+  styleUrls: ['sidebar.component.css'],
+  directives: [BrakcetItemComponent]
 })
 export class SidebarComponent implements OnInit {
 
--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppComponent, environment } from './app/';
+import {HTTP_PROVIDERS} from "@angular/http";
+import {LeguedataService} from "./app/legues/leguedata.service";
 
 if (environment.production) {
   enableProdMode();
 }
 
-bootstrap(AppComponent);
+bootstrap(AppComponent, [
+  LeguedataService,
+  HTTP_PROVIDERS
+]);

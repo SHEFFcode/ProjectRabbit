45f9864831710f62ade27335a1824d1941cc065f
SHEFFcode <sheff@sheffmachine.com>
2016-11-05 14:26:10 -0700

    implement navigation between different matchuos

--- src/app/available-matchups/available-matchups.component.ts
+++ src/app/available-matchups/available-matchups.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import {MatchupItemComponent} from "../matchup-item/matchup-item.component";
+import {Bracket} from "../legues/bracket";
+import {ROUTER_DIRECTIVES, ActivatedRoute, Router} from "@angular/router";
 
 @Component({
   moduleId: module.id,
   selector: 'app-available-matchups',
   templateUrl: 'available-matchups.component.html',
   styleUrls: ['available-matchups.component.css'],
-  directives: [MatchupItemComponent]
+  directives: [MatchupItemComponent, ROUTER_DIRECTIVES]
 })
 export class AvailableMatchupsComponent implements OnInit {
-  private matchups: string[] = ["Josh v Jeremy", "Mike v Steve"];
+  private matchups: Bracket[] = [new Bracket("Josh", "Jeremy"), new Bracket("Mike", "Olga")];
   constructor() { }
 
   ngOnInit() {
--- src/app/matchup-item/matchup-item.component.ts
+++ src/app/matchup-item/matchup-item.component.ts
@@ -1,12 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
+import {Bracket} from "../legues/bracket";
+import {ActivatedRoute, Router, ROUTER_DIRECTIVES} from "@angular/router";
 
 @Component({
   moduleId: module.id,
   selector: 'app-matchup-item',
   templateUrl: 'matchup-item.component.html',
-  styleUrls: ['matchup-item.component.css']
+  styleUrls: ['matchup-item.component.css'],
+  directives: [ROUTER_DIRECTIVES]
 })
 export class MatchupItemComponent implements OnInit {
+
+  @Input() matchup: Bracket;
+  @Input() matchupId: number = 1;
 
   constructor() { }
 
